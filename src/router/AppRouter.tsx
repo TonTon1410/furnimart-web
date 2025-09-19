0d1a5889ef8400f3d783c85ab8898a6174cc54a3
ThuHang2604 <dothuhang2604@gmail.com>
2025-09-19 09:11:03 +0700

    dashboard

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import Cart from "@/pages/Cart";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import NotFound from "@/pages/NotFound";
+import AppLayout from "@/dashboard/AppLayout";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 
@@ -14,6 +15,11 @@ export default function AppRouter() {
       <Route path="/home" element={<Navigate to="/" replace />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="*" element={<NotFound />} />
+
+      <Route path="/dashboard" element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
     </Routes>
   );
 }
